2d366fa184e561b8e18053584853e4db55116834
Himanshu Singh <himanshu7403@gmail.com>
2026-09-05 13:29:47 +0530

    Add MessageRequest.java

--- chatBackend/src/main/java/com/himanshu/chatBackend/payload/MessageRequest.java
+++ chatBackend/src/main/java/com/himanshu/chatBackend/payload/MessageRequest.java
@@ -1,4 +1,20 @@
 package com.himanshu.chatBackend.payload;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MessageRequest {
+    private String content;
+    private String sender;
+    private String roomID;
+//    private LocalDateTime messageTime;
+
 }
